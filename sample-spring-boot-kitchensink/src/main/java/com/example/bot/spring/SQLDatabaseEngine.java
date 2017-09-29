709c807de28e40f3ec9c9dab28c1efa0db34cc02
colinlamty <colinlam422@gmail.com>
2017-09-29 11:10:03 +0800

    lab3 v2.0

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,6 +5,9 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
@@ -13,6 +16,7 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	String search(String text) throws Exception {
 		String result = null;
 		//Write your code here
+		/*
 		Connection connection = getConnection();
 		PreparedStatement stmt  = connection.prepareStatement("Select response from SQLDatabaseEngine where keyword =?"); 
 		stmt.setString(1, text);
@@ -21,6 +25,28 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 			result = rs.getString(1);
 		}
 		return result;
+		*/
+		try {
+			Connection connection = getConnection();
+			PreparedStatement stmt  = connection.prepareStatement("Select response from SQLDatabaseEngine where keyword =?"); 
+			stmt.setString(1, text);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				result = rs.getString(1);
+			}
+		} catch (SQLException e) {
+			log.info("IOException while reading file: {}", e.toString());
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (IOException ex) {
+				log.info("IOException while closing file: {}", ex.toString());
+			}
+		}
+		if (result != null)
+			return result;
+		throw new Exception("NOT FOUND");
 	}
 	
 	
